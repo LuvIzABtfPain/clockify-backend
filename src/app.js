2db2aa13518ac9d28824c23f3f16993810252c6b
Ethan <ethanduong117@gmail.com>
2024-08-07 16:37:07 +0700

    Initial backend Nodejs

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// backend/src/app.js
+const config = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/get-api-key-by-user-id/:userID', (req, res) => {
+    const userID = req.params.userID;
+    db.query('SELECT apikey FROM oncademy_apikey WHERE userID = ?', [userID], (err, results) => {
+        if(err) {
+            res.json(err)
+        } else {
+            res.json({ hasApiKey: results.length > 0 });
+        }
+    });
+});
+
+app.post('/save-api-key', (req, res) => {
+    const { userID, apiKey } = req.body;
+    const query = `
+        INSERT INTO oncademy_apikey (userID, apikey)
+        VALUES (?, ?)
+        ON DUPLICATE KEY UPDATE apikey = VALUES(apikey)
+    `;
+    db.query(query, [userID, apiKey], (err) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.redirect(`http://localhost:3000/apikey=${apiKey}`);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
